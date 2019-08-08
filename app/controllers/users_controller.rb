a253aeb09ce5c8b917b785d62d8e7ef428a62e3b
Samkaa987 <deneubourg.samuel@gmail.com>
2019-08-08 16:09:27 +0200

    fixed signin issue but not post_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,11 @@ class UsersController < ApplicationController
   end
 
   def create
+   puts "£" * 60
+    puts params
     puts "£" * 60
-    puts @user
-    puts "£" * 60
-    city = City.find_or_create_by(name: params[:name])
-    zip_code = City.find_or_create_by(zip_code: params[:zip_code])
-    @user = User.create(first_name: post_params[:first_name], last_name: post_params[:last_name], email: post_params[:email], city: city, zip_code: zip_code, age: post_params[:age], description: post_params[:description], password: post_params[:password], password_confirmation: post_params[:password_confirmation])
+    city = City.find_or_create_by(name: params[:name], zip_code: params[:zip_code])
+    @user = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], city: city, age: params[:age], description: params[:description], password: params[:password], password_confirmation: params[:password_confirmation])
       if @user.save 
           flash[:success] = "Vous êtes inscrits !"
           session[:user_id] = @user.id
@@ -40,7 +39,7 @@ class UsersController < ApplicationController
   private 
 
   def post_params
-    params.require(:user).permit(:first_name, :last_name, :email, :age, :city, :zip_code, :description, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :age, :zip_code, :city, :description, :password, :password_confirmation)
   end
 
 end
